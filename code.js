2012b5e0de6085ae04a5b47d6dd90562faf4b46b
Albin Olausson <abbeolausson@hotmail.com>
2022-11-29 10:38:00 +0100

    copy paste merge

--- code.js
+++ code.js
@@ -8,7 +8,14 @@ function checkAnswer(){
  if(document.getElementById("b2").checked){
     nbr++;
  }
+ if(document.getElementById("c3").checked){
+   nbr++;
+}
+if(document.getElementById("b4").checked){
+   nbr++;
+}
+
  console.log(nbr);
- document.getElementById("point").innerHTML = "Du fick " + nbr + "/2";
+ document.getElementById("point").innerHTML = "Du fick " + nbr + "/4";
  document.getElementById("point").style.color = "red";
 }
